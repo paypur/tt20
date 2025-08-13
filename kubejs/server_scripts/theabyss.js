54a550d39a488eb363731664a59950cf01cf7aee
paypur <pieceofpaypur@gmail.com>
2025-08-12 19:29:04 -0700

    Added missing tags to some of theabyss ores
    Took 10 minutes

---
+++ kubejs/server_scripts/theabyss.js
@@ -0,0 +1,13 @@
+ServerEvents.tags("item", event => {
+    event.add("forge:ores/diamond", "theabyss:diamond_variant");
+    event.add("forge:ores/gold", "theabyss:gold_variant");
+    event.add("forge:ores/iron", "theabyss:iron_variant");
+    event.add("forge:ores/emerald", "theabyss:emerald_variant");
+})
+
+ServerEvents.tags("block", event => {
+    event.add("forge:ores/diamond", "theabyss:diamond_variant");
+    event.add("forge:ores/gold", "theabyss:gold_variant");
+    event.add("forge:ores/iron", "theabyss:iron_variant");
+    event.add("forge:ores/emerald", "theabyss:emerald_variant");
+})
