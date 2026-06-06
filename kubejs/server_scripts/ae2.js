a646b2d689666991cd80dea8edd88f520bfc1bd3
paypur <pieceofpaypur@gmail.com>
2026-06-06 13:38:31 -0700

    Changed some ae2 recipes to silver

--- kubejs/server_scripts/ae2.js
+++ kubejs/server_scripts/ae2.js
@@ -18,5 +18,16 @@ ServerEvents.recipes(event => {
         O: 'ae2:logic_processor'
     })
 
+    /* Non block items, replaced with silver
+     */
+    event.replaceInput({output: "ae2:advanced_card"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:view_cell"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:item_cell_housing"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:item_storage_cell_1k"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:item_storage_cell_4k"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:item_storage_cell_16k"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:item_storage_cell_64k"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+    event.replaceInput({output: "ae2:item_storage_cell_256k"}, "minecraft:iron_ingot", "#forge:ingots/silver");
+
     event.replaceInput({mod: "ae2"}, "minecraft:iron_ingot", "#forge:ingots/aluminum");
 })
